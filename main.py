718c58a81c21654e5ea4603fe833e24f1ef212e1
TanaT <mihail.zubko@yandex.ru>
2024-02-11 03:03:26 +0300

    some fixes

--- main.py
+++ main.py
@@ -1,83 +1,106 @@
 from typing import Union
 from guide import Guide
 
+is_ru: bool = False
 
 class Interface:
-    is_ru: bool = False
-
+    
     def __init__(self) -> None:
-        pass
+        self.lang_choise()
+        self = MainInterface()
 
     def lang_choise(self) -> None:
+        global is_ru
         while True:
             choose: str = input('choose language/выберите язык(eng/ru):').lower()
             if choose in ['eng', 'анг']:
-                self.is_ru: bool = False
+                is_ru = False
                 break
             elif choose in ['ru', 'рус']:
-                self.is_ru: bool = True
+                is_ru = True
                 break
             else:
                 print('unknown command/неизвестная команда')
                 continue
-        print(['english language chosen', 'выбран русский язык'][self.is_ru])
-
-
-class MainInterface(Interface):
-    def __init__(self):
-        pass
-
-
-
-    def main_int(self) -> None:
-        while True:
-            print([f'Here is a guide. Number of records: ',
-                   f'Перед вами справочник. Количество записей: '][self.is_ru], test_guide.length)
-            query: str = input([f'Enter command (help for commands list): ',
-                                f'Введите команду(помощь - список команд): '][self.is_ru]).lower()
-            self.query_process_main(query)
-
-    def query_process_main(self, query):
-        if query in ('read', 'читать'):
-            self.read_guide()
-        else:
-            self.query_process(query)
+        print(['english language chosen', 'выбран русский язык'][is_ru])
 
     def query_process(self, query: 'str') -> None:
         if query in ('exit', 'выход'):
             exit()
         elif query in ('help', 'помощь'):
-            self.help_int()
+            self.help()
         else:
-            print(['unknown command', 'неизвестная команда'][self.is_ru])
+            print(['unknown command', 'неизвестная команда'][is_ru])
 
-    def help_int(self) -> None:
-        print(*[['commands list:\n', 'exit - quit program\n', 'read - open guide\n'],
-                ['список команд:\n', 'выход - завершить программу\n', 'читать - открыть справочник\n']]
-        [self.is_ru])
+    def help(self) -> None:
+        print(*[['commands list:\n', 'exit - quit program\n', 'help - commands list'],
+                ['список команд:\n', 'выход - завершить программу\n', 'помощь - список команд']]
+        [is_ru], sep='')
+
+
+class MainInterface(Interface):
+    def __init__(self):
+        self.main_int()
+
+    def main_int(self) -> None:
+        while True:
+            print([f'Here is a guide. Number of records:',
+                   f'Перед вами справочник. Количество записей: '][is_ru], test_guide.length)
+            query: str = input([f'Enter command (help - commands list):',
+                                f'Введите команду(помощь - список команд): '][is_ru]).lower()
+            self.query_process(query)
+
+    def query_process(self, query):
+        if query in ('read', 'читать'):
+            self = ReadInterface()
+        else:
+            super().query_process(query)
+
+    def help(self) -> None:
+        super().help()
+        print(*[['read - open guide\n'], ['читать - открыть справочник\n']][is_ru])
+
+
+class ReadInterface(Interface):
+    def __init__(self):
+        self.read_guide()
 
     def read_guide(self) -> None:
         now_page: int = 1
-        while True:
-            print([f'Note N: ', f'Запись №: '][self.is_ru], now_page, '\n',
-                  [f'Lastname: ', f'Фамилия: '][self.is_ru], test_guide.notes[now_page]['lastname'], '\n', )
-            now_page: int = self.query_process_read(now_page)
+        flag = True
+        while flag:
+            print([f'Note N: ', f'Запись №: '][is_ru], now_page, '\n',
+                  [f'lastname: ', f'фамилия: '][is_ru], test_guide.notes[now_page]['lastname'], '\n',
+                  [f'name: ', f'имя: '][is_ru], test_guide.notes[now_page]['name'], '\n',
+                  [f'middlename: ', f'отчество: '][is_ru], test_guide.notes[now_page]['middlename'], '\n',
+                  [f'organization: ', f'организация: '][is_ru], test_guide.notes[now_page]['organization'], '\n',
+                  [f'work_number: ', f'рабочий телефон: '][is_ru], test_guide.notes[now_page]['work_number'], '\n',
+                  [f'privat_number: ', f'личный телефон: '][is_ru], test_guide.notes[now_page]['privat_number'],
+                  '\n')
+            now_page, flag = self.query_process(now_page, flag)
 
-    def query_process_read(self, now_page) -> Union[int, None]:
+    def query_process(self, now_page, flag) -> Union[int, None]:
         while True:
-            query: str = input([f'Enter command (n - next, b - previous): ',
-                                f'Введите команду(с - следующая, ч - предыдущая): '][self.is_ru]).lower()
+            query: str = input([f'Enter command (n - next, b - previous, help - commands list): ',
+                                f'Введите команду(с - следующая, ч - предыдущая, помощь - список команд): '][is_ru]).lower()
             if query in ['n', 'с']:
                 now_page += 1
                 break
             elif query in ['b', 'ч']:
                 now_page -= 1
                 break
+            elif query in ['back', 'назад']:
+                flag = False
+                break
             else:
-                self.query_process(query)
+                super().query_process(query)
         now_page: int = abs(now_page) % test_guide.length
         now_page: int = test_guide.length if now_page == 0 else now_page
-        return now_page
+        return now_page, flag
+
+    def help(self) -> None:
+        super().help()
+        print(*[['back - main menu\n'], ['назад - главное меню\n']][is_ru])
 
 
 test_guide: Guide = Guide()
